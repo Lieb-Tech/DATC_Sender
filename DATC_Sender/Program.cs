086088a9264aa6f822b8745ad24cdaa32fe93c70
Marten Liebster <mliebster@lieb-tech.com>
2019-06-05 23:30:05 -0400

    app start up

--- DATC_Sender/Program.cs
+++ DATC_Sender/Program.cs
@@ -12,11 +12,13 @@ namespace DATC_Sender
             using (var sys = Akka.Actor.ActorSystem.Create("DATC"))
             {
                 var eh = Helpers.EventHubConfig.Configuration;
-                //var uploader = sys.ActorOf(Actors.UploadActor.Props(eh.eventHubConnectionString, eh.eventHubName));
-                var uploader = sys.ActorOf(MockUploadActor.Props(), "mockMcMockFace");
+                var uploader = sys.ActorOf(Actors.UploadActor.Props(eh.eventHubConnectionString, eh.eventHubName));
+                // var uploader = sys.ActorOf(MockUploadActor.Props(), "mockMcMockFace");
                 var coord = sys.ActorOf(CoordinatorActor.Props("192.168.1.148", uploader));
 
+                Console.WriteLine("<Enter> to shutdown");
                 Console.ReadLine();
+                Console.WriteLine("<Enter> to confirm");
                 Console.ReadLine();
             }
         }
